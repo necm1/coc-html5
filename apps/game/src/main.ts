e67ab79d39de0ebf24bfa6bae79e90ec88c5d5fb
necm1 <necmi.m.hasan@outlook.de>
2025-06-22 02:52:24 +0200

    chore: clean

--- apps/game/src/main.ts
+++ apps/game/src/main.ts
@@ -1,11 +1,8 @@
-import { ClashCore } from '@coc/core';
+import { ClashCore, ClashWorld } from '@coc/core';
 
 (async () => {
-  // if (typeof window === 'undefined') {
-  //   console.error('This script is intended to run in a browser environment.');
-  //   return;
-  // }
-
   const clash = ClashCore.getInstance();
   await clash.init();
+
+  const world = new ClashWorld();
 })();
